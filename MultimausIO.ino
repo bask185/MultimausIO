787d989ca20076df99211986f522b649db89dd7c
Sebastiaan Knippels <sknippels@hollandmechanics.com>
2021-11-17 10:54:44 +0100

    added weistra files for train control

--- MultimausIO.ino
+++ MultimausIO.ino
@@ -16,37 +16,51 @@ void notifyXNetLocoDrive28( uint16_t Address, uint8_t Speed )  // N.B. it may be
 
 }
 
+void setOutput( uint8_t Address, uint8_t functions )
+{
+    if( address > 3) return ; // discard other addresses
+
+    uint8_t number = 1;
+    if( functions & F5_F8 ) number = 5 ;
+    // address == 1 && F1-F4 -> number range =  1 -  4 -> points  1  -  4
+    // address == 2 && F1-F4 -> number range = 11 - 14 -> points  9  -  12
+    // address == 3 && F1-F4 -> number range = 21 - 24 -> relay   1  -  4
+    // address == 1 && F5-F8 -> number range =  1 -  4 -> points  1  -  4
+    // address == 2 && F5-F8 -> number range = 11 - 14 -> points  9  -  12
+    // address == 3 && F5-F8 -> number range = 21 - 24 -> relay   1  -  4
+    
+
+    for( int bitMask = 0b001 ; bitMask < 0b1000 ; bitMask <<= 1 )
+    {
+        if( (functions & bitMask) != (prevStates[ Address ] & bitMask) )        // check all 4 bits for F1 - F4, if atleast 1 bit has changed
+        {
+            prevStates[ Address ] = functions & 0x0F ;
+
+            uint8_t ioNumber = number + ((Address - 1) * 10) ;
+            uint8_t state ;
+            if( functions & bitMask )   state = 1 ;    // on
+            else                        state = 0 ;    // off
+
+            setOutput( ioNumber, state ) ;
+
+            return ;
+        }
+
+        number ++ ;
+    }
+}
+
 void notifyXNetLocoFunc1( uint16_t Address, uint8_t Func1 )  //Gruppe1 0 0 0 F0 F4 F3 F2 F1
 {
-    // address == 1 -> number range =  1 -  4 -> points  1  -  4
-    // address == 2 -> number range = 11 - 14 -> points  9  -  12
-    // address == 3 -> number range = 21 - 24 -> relay   1  -  4
-    if( address > 3) return ;
-
-    for( int bitMask = 0b001 ; bitMask < 0b1000 ; bitMask <<= 1 )
-    {
-        if( (Func1 & bitMask) != (prevStates[ Address ] & bitMask) )        // check all 4 bits for F1 - F4, if atleast 1 bit has changed
-        {
-            prevStates[ Address ] = Func1 & 0x0F ;
-        }
-    }
+    setOutput( Address, Func1, F1-F4 ) ;
 }
+
+   
 void notifyXNetLocoFunc2( uint16_t Address, uint8_t Func2 )  //Gruppe2 0000 F8 F7 F6 F5
 {
-    // address == 1 -> number range =  5 -  8 -> points  5  -  8
-    // address == 2 -> number range = 15 - 18 -> points 13  - 16
-    // address == 3 -> number range = 25 - 28 -> relay   5  -  8
-    if( address > 3) return ;
-
-    for( int bitMask = 0b001 ; bitMask < 0b1000 ; bitMask <<= 1 )
-    {
-        if( (Func2 & bitMask) != (prevStates[Address + 3] & bitMask) )        // check all 4 bits for F5 - F4
-        {
-            prevStates[ Address + 3 ] = Func2 & 0x0F ;                          // store the change
-        }
-    }
-    
+    setOutput( Address, Func2 | F5-F8 ) ;
 }
+
 
 void setup()
 {
